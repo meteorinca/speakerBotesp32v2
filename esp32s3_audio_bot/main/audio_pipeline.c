dd05b6cd1940ee6b863f08691732827ed06097fa
meteorinca <meteorinca@gmail.com>
2026-03-25 23:09:36 -0400

    works

--- esp32s3_audio_bot/main/audio_pipeline.c
+++ esp32s3_audio_bot/main/audio_pipeline.c
@@ -21,11 +21,21 @@ void audio_pipeline_push_pcm(const uint8_t *data, uint16_t len) {
 
 static void i2s_tx_task(void *arg) {
     size_t item_size = 0;
+    bool was_playing = false;
     while (1) {
         uint8_t *item = (uint8_t *)xRingbufferReceive(g_pcm_play_rb, &item_size, pdMS_TO_TICKS(100));
         if (item) {
             audio_i2s_play_pcm(item, item_size);
             vRingbufferReturnItem(g_pcm_play_rb, item);
+            was_playing = true;
+        } else if (was_playing) {
+            // Flush DMA buffers with zeroes to prevent DC offset scratch/buzz sounds
+            uint8_t *silence = calloc(1, 4096);
+            if (silence) {
+                audio_i2s_play_pcm(silence, 4096);
+                free(silence);
+            }
+            was_playing = false;
         }
     }
 }
